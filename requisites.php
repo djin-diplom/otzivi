ffeaa789d2414deb06a88ce2114bf5ffa820c917
21vek <git@github.com>
2017-08-01 14:30:56 +0300

    1

---
+++ requisites.php
@@ -0,0 +1,10 @@
+<?php
+
+date_default_timezone_set('Europe/Moscow');
+$datetime_site = date("Y-m-d H:i:s");
+
+$main_name = "/projects/landingpages/otzivi";
+$donor_name = 'http://xn--d1acynfdde.xn--p1ai';
+$donor_site_name = 'Студенту.рф';
+//$main_name_corot = "BYPolit.org";
+$site_name = "Вива-диплом";
---
+++ router.php
@@ -0,0 +1,24 @@
+<?php
+
+$REQUEST_URI = $_SERVER['REQUEST_URI'];
+
+$contentTitle = str_replace($main_name, $donor_name, $REQUEST_URI );
+
+$mainContent = file_get_contents($contentTitle);
+
+$mainContent = str_replace('src="', 'src="'.$donor_name, $mainContent  );
+$mainContent = str_replace('href="/templates/', 'href="'.$donor_name.'/templates/', $mainContent  );
+
+//$mainContent = str_replace($donor_name, $main_name, $mainContent  );
+$mainContent = str_replace($donor_site_name, $site_name, $mainContent  );
+//$mainContent = str_replace('/engine/classes/js/', './index_files/', $mainContent  );
+//$mainContent = str_replace('/engine/classes/', './index_files/', $mainContent  );
+//$mainContent = str_replace('/templates/city-skyline/assets/css/', './index_files/', $mainContent  );
+
+
+//$mainContent = str_replace('/templates/city-skyline/assets/js/', './index_files/', $mainContent  );
+//$mainContent = str_replace('/templates/city-skyline/style/', './index_files/', $mainContent  );
+//$mainContent = str_replace('/favicon.png', './favicon.ico', $mainContent  );
+
+
+echo $mainContent;
