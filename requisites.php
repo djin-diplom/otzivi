5cc8312f3485e224e7d61721b592c116def8ab9d
21vek <git@github.com>
2017-08-02 16:24:04 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://studrabota.ru';//"/projects/landingpages/otzivi";
+$main_name = 'http://studrabota.com/';//"/projects/landingpages/otzivi";
 $donor_name = 'http://xn--d1acynfdde.xn--p1ai';
 $donor_site_name = 'Студенту.рф';
 //$main_name_corot = "BYPolit.org";
