bf0c1b04dc21a33ec3cf82475a2308122eca8ae4
21vek <git@github.com>
2017-09-16 21:42:30 +0300

    1

--- router.php
+++ router.php
@@ -17,6 +17,8 @@ $REQUEST_URI = $main_name.$REQUEST_URI;
 
 $REQUEST_URI = str_replace( '177-rosdiplom.html', '', $REQUEST_URI  );//new
 
+$REQUEST_URI = str_replace( '177', '', $REQUEST_URI  );//new
+
 $REQUEST_URI = str_replace( '1001-znaniumbaza_ru', '177-rosdiplom_ru', $REQUEST_URI  );//new
 
 //$REQUEST_URI = str_replace( '1001', '177', $REQUEST_URI  );//new
