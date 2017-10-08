51f27188af25f9adc9411542e7da829ba64425ff
21vek <git@github.com>
2017-10-08 19:08:07 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://vivastudent.ru/';
+$main_name = 'http://vivastudent.ru';
 $donor_name = 'http://xn--d1acynfdde.xn--p1ai';
 $donor_site_name = 'Студенту.рф';
 $site_name = "Студенческие работы";
