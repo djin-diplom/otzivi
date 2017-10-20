0d7b9092c1f813b5df940daf20cee9026d375bd1
21vek <git@github.com>
2017-10-20 21:51:32 +0300

    1

--- requisites.php
+++ requisites.php
@@ -17,4 +17,32 @@ $raskrutka_site_4 = 'bazaznanij_com';
 $raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">ул. Ленина, дом № 28, oф. 56';
 
 $yandex_google = '<meta name="yandex-verification" content="c501f550de5d69b6" />
-<meta name="google-site-verification" content="nYwj09iau7v7Ip_GwGkyoRskEb1IHPTjWfv-QUb9m2I" />';
+<meta name="google-site-verification" content="nYwj09iau7v7Ip_GwGkyoRskEb1IHPTjWfv-QUb9m2I" />
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter46355961 = new Ya.Metrika({
+                    id:46355961,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/46355961" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->';
