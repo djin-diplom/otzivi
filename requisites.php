cfcf7128de7a297e57266ff261645d547b2b79fe
21vek <git@github.com>
2017-11-03 11:41:50 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,46 +3,19 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://vivastudent.ru';
+$main_name = 'http://vivamagister.ru';//'http://vivastudent.ru';
 $donor_name = 'http://xn--d1acynfdde.xn--p1ai';
 $donor_site_name = 'Студенту.рф';
 $site_name = "Студенческие работы";
 
 
-$raskrutka_site_1 = 'bazaznanij.com';
-$raskrutka_site_2 = 'Bazaznanij';
-$raskrutka_site_3 = 'BAZAZNANIJ.COM';
-$raskrutka_site_4 = 'bazaznanij_com';
+$raskrutka_site_1 = 'bazazachet.ru';//'bazaznanij.com';
+$raskrutka_site_2 = 'Bazazachet';//'Bazaznanij';
+$raskrutka_site_3 = 'BAZAZACHET.RU';//'BAZAZNANIJ.COM';
+$raskrutka_site_4 = 'bazazachet_ru';//'bazaznanij_com';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">ул. Ленина, дом № 28, oф. 56';
+$raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">Каменноостровский пр-кт, дом № 27, oф. 144-145';
 
 $yandex_google = '<meta name="yandex-verification" content="c501f550de5d69b6" />
 <meta name="google-site-verification" content="nYwj09iau7v7Ip_GwGkyoRskEb1IHPTjWfv-QUb9m2I" />
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter46355961 = new Ya.Metrika({
-                    id:46355961,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true
-                });
-            } catch(e) { }
-        });
-
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/46355961" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->';
+';
