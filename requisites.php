7a82d7dae0fb17e0d6e4c51b41127e62239e5c77
21vek <git@github.com>
2017-11-29 12:24:35 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,18 +3,18 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://vivamagister.ru';//'http://vivastudent.ru';
+$main_name = 'http://viva-magisters.ru';
 $donor_name = 'http://xn--d1acynfdde.xn--p1ai';
 $donor_site_name = 'Студенту.рф';
 $site_name = "Студенческие работы";
 
 
-$raskrutka_site_1 = 'bazazachet.ru';//'bazaznanij.com';
-$raskrutka_site_2 = 'Bazazachet';//'Bazaznanij';
-$raskrutka_site_3 = 'BAZAZACHET.RU';//'BAZAZNANIJ.COM';
-$raskrutka_site_4 = 'bazazachet_ru';//'bazaznanij_com';
+$raskrutka_site_1 = 'basisznanij.ru';//'bazaznanij.com';
+$raskrutka_site_2 = 'Basisznanij';//'Bazaznanij';
+$raskrutka_site_3 = 'BASISZNANIJ.RU';//'BAZAZNANIJ.COM';
+$raskrutka_site_4 = 'basisznanij_ru';//'bazaznanij_com';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">Каменноостровский пр-кт, дом № 27, oф. 144-145';
+$raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">ул. Пушкина, дом № 15, oф. 104-105';
 
 $yandex_google = '<meta name="yandex-verification" content="c501f550de5d69b6" />
 <meta name="google-site-verification" content="nYwj09iau7v7Ip_GwGkyoRskEb1IHPTjWfv-QUb9m2I" />
