78dec97a0b35fdad3ef7a45568bd9aac5cc8ef0a
21vek <git@github.com>
2018-02-10 16:33:15 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,19 +3,19 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://knowbaza.ru';
+$main_name = 'http://bazaznanij.com';
 $donor_name = 'http://xn--d1acynfdde.xn--p1ai';
 $donor_site_name = 'Студенту.рф';
 $site_name = "Студенческие работы";
 
 
-$raskrutka_site_1 = 'vivastudent.ru';//'bazaznanij.com';
-$raskrutka_site_2 = 'Vivastudent';//'Bazaznanij';
-$raskrutka_site_3 = 'VIVASTUDENT.RU';//'BAZAZNANIJ.COM';
-$raskrutka_site_4 = 'vivastudent_ru';//'bazaznanij_com';
+$raskrutka_site_1 = 'diplomfabrika.ru';//'bazaznanij.com';
+$raskrutka_site_2 = 'Diplomfabrika';//'Bazaznanij';
+$raskrutka_site_3 = 'DIPLOMFABRIKA.RU';//'BAZAZNANIJ.COM';
+$raskrutka_site_4 = 'diplomfabrika_ru';//'bazaznanij_com';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">ул. Льва Толстого, дом № 19, oф. 45-46';
+$raskrutka_site_adress = 'г. Санкт-Петербург</span>, <span itemprop="streetAddress">ул. Жуковского, дом № 14, oф. 125-126';
 
-$yandex_google = '<meta name="yandex-verification" content="edfc174bb82c7c5a" />
-<meta name="google-site-verification" content="duABtgxqKHgwREscO6XqQKdLWrKXC2UEQDuTkGuCcJg" />
+$yandex_google = '<meta name="yandex-verification" content="45de6e082524ed27" />
+<meta name="google-site-verification" content="pVSW6pVb_5dlBJ5Zuc19jXuNo5MjfzgZNbZt6Q2EE9U" />
 ';
